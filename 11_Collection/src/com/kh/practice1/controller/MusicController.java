a2933e3a3cb0cfd7386e0f2641f012e4d94b8afe
Viperse <129846485+Viperse@users.noreply.github.com>
2023-06-05 14:55:30 +0900

    23.06.05

---
+++ 11_Collection/src/com/kh/practice1/controller/MusicController.java
@@ -0,0 +1,80 @@
+package com.kh.practice1.controller;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+import com.kh.practice1.compare.ArtistAscending;
+import com.kh.practice1.compare.TitleAscending;
+import com.kh.practice1.model.Music;
+
+public class MusicController {
+	
+	ArrayList<Music> list = new ArrayList<>();
+	
+	public boolean addList(Music music) {
+		
+		return list.add(music);
+	}
+	
+	public boolean addAtZero(Music music) {		
+		try {
+			list.add(0, music);
+			return true;
+		} catch(Exception e) {
+			return false;
+		}
+		
+	}
+	
+	public ArrayList<Music> printAll() {
+		return list;
+	}
+	
+	public Music searchMusic(String title) {
+		for(int i=0; i<list.size(); i++) {
+			if(list.get(i).getTitle().contains(title)) {
+				return list.get(i);
+			}
+			
+		}
+		return null;
+	}
+	
+	public Music removeMusic(String title) {
+		for(int i=0; i<list.size(); i++) {
+			if(list.get(i).getTitle().equals(title)) {
+				return list.remove(i);
+				
+			}
+		}
+		
+		return null;
+	}
+	
+	public Music setMusic(String title, Music music) {
+		for(int i=0; i<list.size(); i++) {
+			if(list.get(i).getTitle().equals(title)) {
+				return list.set(i, music);
+			}
+		}
+		
+		return null;
+	}
+	
+	public ArrayList<Music> ascTitle() {
+		
+		ArrayList<Music> list = (ArrayList<Music>)this.list.clone();	
+		// 오름차순
+		Collections.sort(list, new TitleAscending());
+		return list;
+	}
+	
+	public ArrayList<Music> descArtist() {
+		
+		ArrayList<Music> list = (ArrayList<Music>)this.list.clone();
+		// 내림차순
+		Collections.sort(list, new ArtistAscending().reversed());
+		return list;
+	}
+
+}
