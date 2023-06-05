45c3cfe47a63e93354bf65aedc99fd53fc49e5ea
Viperse <129846485+Viperse@users.noreply.github.com>
2023-06-05 10:49:32 +0900

    23.06.05

---
+++ 11_Collection/src/com/kh/list/B_Other.java
@@ -0,0 +1,15 @@
+package com.kh.list;
+
+public class B_Other {
+	
+	public static void main(String[] args) {
+		B_Other b = new B_Other();
+		b.method1();
+	}
+	
+	// Vector
+	public void method1() {
+		
+	}
+
+}
