eb306a3844534a3712ee7fbab8933b1b6f7847a2
Viperse <129846485+Viperse@users.noreply.github.com>
2023-06-08 15:02:24 +0900

    23.06.08

--- Youtube/src/com/youtube/controller/VideoController.java
+++ Youtube/src/com/youtube/controller/VideoController.java
@@ -1,5 +1,7 @@
 package com.youtube.controller;
 
+import java.util.ArrayList;
+
 import com.youtube.model.Video;
 
 public class VideoController {
@@ -10,29 +12,29 @@ public class VideoController {
 	 * Update : 수정
 	 * Delete : 삭제
 	 */
+	ArrayList<Video> videoList = new ArrayList<>();
 	
-	Video[] videoList = new Video[5];
-	int index = 0;
+	
 	public void upload(Video video) { // 영상 업로드	
 		
-		videoList[index++] = video;		
+		videoList.add(video);
 	}
 	
-	public Video[] videoList() { // 영상 목록
+	public ArrayList<Video> videoList() { // 영상 목록
 		return videoList;
 	}
 
 	public Video viewVideo(int index) { // 영상 1개 보기
-		return videoList[index];
+		return videoList.get(index);
 	}
 
 	public void updateVideo(int index, Video video) { // 동영상 수정
 		
-		videoList[index] = video;
+		videoList.set(index, video);
 	}
 
-	public boolean deleteVideo() { // 동영상 삭제
-		return false;
+	public boolean deleteVideo(Video video) { // 동영상 삭제
+		return videoList.remove(video);
 	}
 
 }
