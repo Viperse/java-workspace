bd09920545dcd611e4e4f4c3abd1edecff45a12f
Viperse <129846485+Viperse@users.noreply.github.com>
2023-06-20 17:48:19 +0900

    fix: 코테 문제

--- Test/src/Solution.java
+++ Test/src/Solution.java
@@ -1,8 +1,20 @@
-class Solution {
-	
-    public int[] solution(String[] name, int[] yearning, String[][] photo) {
-    	
-        int[] answer = {};
-        return answer;
+import java.util.Arrays;
+
+class Solution {	
+    public String solution(String my_string, int num1, int num2) {
+        
+    	char ch1 = my_string.charAt(num1);
+    	char ch2 = my_string.charAt(num2);
+    	  	
+        return null;
     }
+    
+    public static void main(String[] args) {
+		Solution s = new Solution();
+		String my1 = "hello";
+		String my2 = "I love you";
+		
+		System.out.println(s.solution(my1, 1, 2));
+		System.out.println(s.solution(my2, 3, 6));
+	}
 }
